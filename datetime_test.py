3dc66ec95a95d3619cfce2b618b95cedb1ec919c
cullen <cullen.dolan@gmail.com>
2021-01-31 18:15:57 -0600

    creates a list of slots given a start end end date

--- datetime_test.py
+++ datetime_test.py
@@ -5,20 +5,18 @@ def main():
     # define all the varibles (what probably gets passed in)
     year_start, month_start, day_start = 2018, 1, 1
     hour_start, minute_start = 6, 0
-    year_end, month_end, day_end = 2018, 12, 31
+    year_end, month_end, day_end = 2023, 12, 31
     hour_end, minute_end = 18, 0
     work_hours_per_day = 12
     slot_duration_int = 5
 
-    # define the dates and datetimes
+    # define the  datetimes
     start_datetime = datetime(year_start, month_start, day_start, hour_start, minute_start)
-    start_date = date(year_start, month_start, day_start)
     end_datetime = datetime(year_end, month_end, day_end, hour_end, minute_end)
-    end_date = date(year_end, month_end, day_end)
-    
     
     slots_per_day = define_slots(work_hours_per_day, slot_duration_int)
     slot_list = return_slot_list(start_datetime, end_datetime, slot_duration_int, slots_per_day)
+    print(len(slot_list))
 
 
 def return_slot_list(start_datetime, end_datetime, slot_duration_int, slots_per_day):
@@ -27,7 +25,7 @@ def return_slot_list(start_datetime, end_datetime, slot_duration_int, slots_per_
     
     while start_datetime <= end_datetime:
         slot = 0
-        print(start_datetime)
+        #print(start_datetime)
         while slot < slots_per_day:
             slot_list.append(datetime.strftime(start_datetime, '%Y-%m-%d %H:%M'))
             slot += 1
