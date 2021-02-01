05947ae0951194c32d0001af03ff953617f64265
cullen <cullen.dolan@gmail.com>
2021-01-31 18:12:56 -0600

    creates a list of slots given a start end end date

--- datetime_test.py
+++ datetime_test.py
@@ -5,7 +5,7 @@ def main():
     # define all the varibles (what probably gets passed in)
     year_start, month_start, day_start = 2018, 1, 1
     hour_start, minute_start = 6, 0
-    year_end, month_end, day_end = 2018, 1, 2
+    year_end, month_end, day_end = 2018, 12, 31
     hour_end, minute_end = 18, 0
     work_hours_per_day = 12
     slot_duration_int = 5
@@ -19,15 +19,15 @@ def main():
     
     slots_per_day = define_slots(work_hours_per_day, slot_duration_int)
     slot_list = return_slot_list(start_datetime, end_datetime, slot_duration_int, slots_per_day)
-    print(slot_list)
 
 
 def return_slot_list(start_datetime, end_datetime, slot_duration_int, slots_per_day):
-    # return the list of slots per day
+    # return the list of slots per day given the number of days
     slot_list = []
     
     while start_datetime <= end_datetime:
         slot = 0
+        print(start_datetime)
         while slot < slots_per_day:
             slot_list.append(datetime.strftime(start_datetime, '%Y-%m-%d %H:%M'))
             slot += 1
@@ -36,17 +36,10 @@ def return_slot_list(start_datetime, end_datetime, slot_duration_int, slots_per_
     return slot_list
 
 
-def days_on_schedule(start_date, end_date,delta):
-    curr = start_date
-    while curr <= end_date:
-        yield curr
-        curr += delta 
-
 def define_slots(work_hours_per_day, slot_duration_int):
     # slots per hour * number of working hours
     mins_in_hr = 60
-    slots_per_day =  mins_in_hr / slot_duration_int * work_hours_per_day
-    return slots_per_day
+    return mins_in_hr / slot_duration_int * work_hours_per_day
 
 
 if __name__ == '__main__':
