335a0c5c4f84b39923aa3046528deae3554d6141
cullen <cullen.dolan@gmail.com>
2021-02-09 23:06:51 -0600

    testing dictionary functionality

---
+++ datetime_dict.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+new_dict = {datetime(2021,2,9,6):123, datetime(2021,2,9,6,5):0, datetime(2021,2,9,6,10):456}
+
+print(new_dict)
+
+ask_year = int(input('Year: '))
+ask_month = int(input('Month: '))
+ask_day = int(input('Day: '))
+ask_hour = int(input('Hour: '))
+ask_min = int(input('Min: '))
+ask_provider_id = int(input('Provider ID: '))
+
+if new_dict[datetime(ask_year, ask_month, ask_day, ask_hour, ask_min)] == 0:
+    new_dict[datetime(ask_year, ask_month, ask_day, ask_hour, ask_min)]=ask_provider_id
+else:
+    print('That time slot is taken by ' + str(new_dict[datetime(ask_year, ask_month, ask_day, ask_hour, ask_min)]))
+
+print(new_dict)
--- location_schedule.py
+++ location_schedule.py
@@ -2,7 +2,7 @@ from get_slots import get_time_slots
 
 class building_one_slots:
     def __init__(self, allocated_time_slots, allocation):
-        self.allocated_time_slots = get_time_slots()
+        self.allocated_time_slots = allocated_time_slots
         self.allocation = allocation
 
 
