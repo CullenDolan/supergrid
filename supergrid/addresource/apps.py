0488f670ee1c5ebee0dc61b66365f5e51ae17c5e
cullen <cullen.dolan@gmail.com>
2021-02-10 22:03:20 -0600

    started resource app

---
+++ supergrid/addresource/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AddresourceConfig(AppConfig):
+    name = 'addresource'
--- supergrid/supergrid/settings.py
+++ supergrid/supergrid/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rg8bwr$&l4@!09nuh1)8ias@2!pl7erouh86-m5_tq^1e74r$7'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
